551dcc4dc166b0ade4d7964f7ae6d642629623ca
Jordi Deu-Pons <jordi.deu@irbbarcelona.org>
2017-10-18 12:23:02 +0200

    Add chromosome statistics after vep

--- filters/vep.py
+++ filters/vep.py
@@ -53,5 +53,8 @@ class VepFilter(Filter):
         if count_after == 0:
             self.stats[group_key]["error_no_entries"] = "There is no VEP output"
 
-        if len(chromosomes) < 23:
+        if len(chromosomes) < 20:
             self.stats[group_key]["error_few_chromosomes_with_mutations"] = "There are only {} chromosomes with mutations"
+        elif len(chromosomes) < 23:
+            self.stats[group_key]["warning_few_chromosomes_with_mutations"] = "There are only {} chromosomes with mutations"
+
