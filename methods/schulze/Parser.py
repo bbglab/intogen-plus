7781ee0e5c1d8221190373d6f31ad3fb8f4a2fa8
Jordi Deu-Pons <jordi.deu@irbbarcelona.org>
2018-03-15 15:55:57 +0100

    Update cbase headers

--- methods/schulze/Parser.py
+++ methods/schulze/Parser.py
@@ -16,7 +16,7 @@ class Parser():
         "oncodrivefml":         ["SYMBOL",      "Q_VALUE",      "P_VALUE"],
         "dndscv":               ["gene_name",   "qallsubs_cv",  "pallsubs_cv"],
         "edriver":              ["SYMBOL",      "QVALUE",       "PVALUE"],
-        "cbase":                ["gene",        "q_phi_pos",    "p_phi_pos"],
+        "cbase":                ["gene",        "q_pos",    "p_pos"],
         "oncodriveclustl":      ["SYMBOL",         "Q_ANALYTICAL",       "P_ANALYTICAL"]
         # "oncodriveclust":       ["SYMBOL",      "QVALUE",       "PVALUE"],
         # "mutsigcv": ["gene","q", "p"],
