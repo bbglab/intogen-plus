7c9fc9699320f86c7298296b525a5b55235acb46
Jordi Deu-Pons <jordi.deu@irbbarcelona.org>
2018-03-15 18:05:00 +0100

    Use two mutrate environments

--- methods/mutrate/pipeline.sh
+++ methods/mutrate/pipeline.sh
@@ -4,7 +4,6 @@
 # ./pipeline.sh <annotmuts_path> <genemuts_path> <mcmc_iterations> <output_folder> <cores>
 # Example: ./pipeline.sh ./annotmuts.tsv ./genemuts.tsv 1000 ./output/BRCA_TCGA 30 
 
-source activate intogen2017_mutrate
 
 ANNOTMUTS=$1
 GENEMUTS=$2
@@ -18,6 +17,8 @@ export PYTHONPATH=${SCRIPTS_FOLDER}:${PYTHONPATH}
 # STEP1. Prepare files for sigfit to do signature fitting
 echo "## STEP1: Preparing files for sigfit..."
 mkdir -p ${OUTPUT_FOLDER}
+source activate intogen2017_mutrate_python
+
 if python -W ignore ${SCRIPTS_FOLDER}/mutrate.py \
                     sigfit -a ${ANNOTMUTS} \
                            -o ${OUTPUT_FOLDER}/mutational_catalogue.tsv; then
@@ -30,6 +31,7 @@ fi
 # STEP2. Run signature fitting with the full catalogue
 echo "## STEP2: Running sigfit..."
 mkdir -p ${OUTPUT_FOLDER}/sigfit_results
+source activate intogen2017_mutrate_r
 if Rscript --vanilla ${SCRIPTS_FOLDER}/cosmic_exome_fit.r -m ${OUTPUT_FOLDER}/mutational_catalogue.tsv \
                                                              -c ${SCRIPTS_FOLDER}/data/cosmic_exome.tsv \
                                                              -i ${ITERATIONS} \
@@ -43,6 +45,7 @@ fi
 # STEP3. Compute mut expectation per bp as {gene : {sample : {context : expectation}}}
 echo "## STEP3: Compute mut expectation per bp as {gene : {sample : {context : expectation}}}"
 mkdir -p ${OUTPUT_FOLDER}/genes
+source activate intogen2017_mutrate_python
 if python -W ignore ${SCRIPTS_FOLDER}/mutrate.py \
                     compute_mutrate -a ${ANNOTMUTS} \
                                     -g ${GENEMUTS} \
