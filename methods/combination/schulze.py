7117e1c0d09498929111464b1d53076a651fc56b
franmj <francisco.martinez@irbbarcelona.org>
2019-01-03 13:13:48 +0100

    Include the option of Borda Ranking in the Schulze's combination

--- methods/combination/schulze.py
+++ methods/combination/schulze.py
@@ -82,13 +82,14 @@ def strongest_paths_by_chunk(all_candidates, spath, chunk):
     return spath
 
 
-def read_optimized_dicts(optimized_pickles, d_results, output_file, output_dict):
+def read_optimized_dicts(optimized_pickles, d_results, output_file, output_dict, borda=False):
     """
     Generate the optmized ranking by the weights calculated by the opmitzer
     :param optimized_pickles: path of the outputs of the optimizer
     :param d_results: dictionary of results of the individual methods
     :param output_dir: directory of the output of the reports of individual cohorts
     :param output_dict: location of the output directory
+    :param borda: whether to include the borda ranking and score in the output
     :param output_report: location of the output report
     :return: None
     """
@@ -105,11 +106,62 @@ def read_optimized_dicts(optimized_pickles, d_results, output_file, output_dict)
     print(dict_optimal_weights)
     ranking1 = combination_ranking(d_results, dict_optimal_weights)
     df = summary.output_to_dataframe(ranking1, d_results)
-
+    if borda:
+        d, d_scores = get_ranking_borda(d_results)
+        df["RANKING_BORDA"] = df.apply(lambda row: apply_ranking_borda(d, row), axis=1)
+        df["SCORE_BORDA"] = df.apply(lambda row: d_scores[row["SYMBOL"]] if row["SYMBOL"] in d_scores else np.nan,
+                                     axis=1)
     df.sort_values("RANKING").to_csv(output_file, sep="\t", index=False, compression="gzip")
 
     with gzip.open(output_dict, "wb") as fd:
         pickle.dump(ranking1, fd)
+
+def apply_ranking_borda(d,row):
+    '''
+
+    :param d: dictionary of rankings
+    :param row: the row of the dataframe
+    :return: the ranking of the symbol
+    '''
+
+    if row["SYMBOL"] in d:
+        return d[row["SYMBOL"]]
+    else:
+        return np.nan
+
+def get_ranking_borda(d_results):
+    '''
+
+    :param d_results: Dictionary of rankings  for each individual method
+    :return: dictionary of combined rankings using Borda
+    '''
+    d_scores = {}
+    d_len = {}
+    # Create a dictionary of number of total elements per method
+    for method in d_results.keys():
+        #d_len[method]= len(d_results[method].keys()) # This is not fair, it penalizes methods with lower number of candidate genes
+        d_len[method] = 40 # Number of genes fetch to create the pool of candidate genes
+    # Now for each gene, sum the score for that gene
+    for method in d_results.keys():
+        for gene in d_results[method].keys():
+            if not(gene in d_scores):
+                d_scores[gene] = 0
+            d_scores[gene] += d_len[method] - (d_results[method][gene]+1)
+
+    # Sort dictionary by values, the higher the better
+    s_data = sorted(d_scores.items(), key=lambda item: item[1],reverse=True)
+    rank, count, previous, result, score = 0, 0, None, {}, {}
+    for key, num in s_data:
+        count += 1
+        if num != previous:
+            rank += count
+        previous = num
+        count = 0
+        result[key] = rank
+
+
+    return result, d_scores
+
 
 
 def run_default_weights(d_results, output_dir, output_dict):
@@ -172,13 +224,14 @@ def read_optimized_dicts_cv(dir_optimized_pickles, d_results, output_dir, output
 @click.option('--report_output', type=click.Path(), help="Output reports file",required=True)
 @click.option('--dict_output', type=click.Path(),help="Output dictionary file", required=True)
 @click.option('--type_run',help="Type of run. Optimization of the weights or default wegihts. [default,optimization,cross_validation]", required=True, default="optimization")
-def run_schulze(input_data, optimize_weights, report_output, dict_output, type_run):
+@click.option('--borda',help="Whether to include borda ranking in the output. Default False.", required=False, default=False)
+def run_schulze(input_data, optimize_weights, report_output, dict_output, type_run, borda):
 
     with gzip.open(input_data, "rb") as fd:
         d_results = pickle.load(fd)
 
     if type_run == "optimization":
-        read_optimized_dicts(optimize_weights, d_results, report_output, dict_output)
+        read_optimized_dicts(optimize_weights, d_results, report_output, dict_output, borda)
 
     elif type_run == "cross_validation":
         read_optimized_dicts_cv(optimize_weights, d_results, report_output, dict_output)
