c0fdec2f59b1e1cfd081076405b3b03f1860e17d
fmuinos <ferran.muinos@irbbarcelona.org>
2019-01-22 18:48:46 +0100

    Clean up grid_optimizer.py

--- containers_build/combination/src/grid_optimizer.py
+++ containers_build/combination/src/grid_optimizer.py
@@ -7,47 +7,43 @@ import pandas as pd
 import itertools
 from scipy.optimize import basinhopping
 from functools import partial
+
 from qc.drivers import CGC_GENES_PER_TUMOR, NEGATIVE_GENES_PER_TUMOR
 from qc.deviations import Deviation
 from qc.parser import Parser
 from schulze_election import combination_ranking
 from evaluation.Evaluation_Enrichment import Evaluation_Enrichment
 
-gavaliable_methods = None
-gdiscarded_methods = None
+from configobj import ConfigObj
 
-order_methods_ranking = ["oncodriveclustl", "dndscv","oncodrivefml", "hotmaps","smregions","cbase"]
-order_methods = []
-gweights = []
-gt_combination = None
-method_optimization = None
-null_dict =  {
-            'KTP': 0,
-            'KFP': np.nan,
-            'QQPLOT_STD_HALF': 0,
-            'QQPLOT_SLOPE_HALF': 0,
-            'AREA_RANKING_ABSOLUTE': 0,
-            'AREA_RANKING_RELATIVE': 0
-        }
+# Global variables
+CODE_DIR = os.path.join(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+configs_file = os.path.join(CODE_DIR, 'src', 'config', 'intogen_qc.cfg')
+configs = ConfigObj(configs_file)
+METHODS = configs["methods"].keys() # reads them in order
+
+gavaliable_methods = None
+
 PATH_REGIONS = os.path.join(os.environ['INTOGEN_DATASETS'], 'shared', 'cds.regions.gz')
-METHODS = [
-    'oncodrivefml',
-    'oncodriveclustl',
-    'dndscv',
-    'hotmaps',
-    'cbase',
-    'smregions']
+
 
 def get_tissue(name):
-    '''
-
+    """
     :param name:
     :return:
-    '''
+    """
     for t in NEGATIVE_GENES_PER_TUMOR.keys():
         if "_{}_".format(t) in name or name.endswith("_{}".format(t)):
             return t
     return None
+
 
 class Filter:
 
@@ -70,23 +66,10 @@ class Filter:
         if df is None or len(df) == 0:
             return None
 
-        positive = df[df['QVALUE'] < 0.1]
-        positive = set(positive['SYMBOL']) if len(positive) > 0 else set()
-        true_positive = positive & CGC_GENES_PER_TUMOR['PANCANCER']
-        try:
-            false_positive = positive & NEGATIVE_GENES_PER_TUMOR[get_tissue(tumor_name)]
-        except KeyError as err:
-            false_positive = None
-
         qc = Deviation(df=df, description=parser.name)
-        deviation_obs_half = qc.deviation_from_null(half=True)
         areas = qc.calculate_areas()
 
         result = {
-            'KTP': len(true_positive),
-            'KFP': np.nan if false_positive is None else len(false_positive),
-            'QQPLOT_STD_HALF': deviation_obs_half['deviation'],
-            'QQPLOT_SLOPE_HALF': deviation_obs_half['slope'],
             'AREA_RANKING_ABSOLUTE': areas['absolute'],
             'AREA_RANKING_RELATIVE': areas['relative']
         }
@@ -107,7 +90,10 @@ class Filter:
             if res is not None:
                 results[method] = res
             else:
-                results[method] = null_dict
+                results[method] = {
+                    'AREA_RANKING_ABSOLUTE': 0,
+                    'AREA_RANKING_RELATIVE': 0
+                }
 
         return pd.DataFrame(results).T
 
@@ -146,7 +132,7 @@ class Filter:
         return self.filter_by(by='AREA_RANKING_ABSOLUTE') & self.filter_by(by='AREA_RANKING_RELATIVE')
 
 
-def calculate_objective_function(d_ranking, weights, objective_method="Combination_Ranking", objective_function=None):
+def calculate_objective_function(d_ranking, weights, objective_function, objective_method="Combination_Ranking"):
     """
     Given a distribution of weights returns the value of the objective function
     (default: enrichment, combination_ranking)
@@ -161,17 +147,9 @@ def calculate_objective_function(d_ranking, weights, objective_method="Combinati
     :return float of the current value of the objective function for the input weights.
     """
 
-    global gweights
     ranking = combination_ranking(d_ranking, weights)
-
-    d_f = dict()
-    d_f[objective_method] = dict(ranking)
-    if gt_combination == "RANKING":
-        type = "absolute"
-    else:
-        type = "relative"
-    d_area = objective_function.calculate_area(d_f, type_method=type)
-
+    d_f = {objective_method: dict(ranking)}
+    d_area = objective_function.calculate_area(d_f, type_method="absolute")
     return d_area[objective_method]
 
 
@@ -182,13 +160,10 @@ def prepare_output(methods_order, solutions):
     :return: list of solutions ready to be created by DataFrame
     """
     l_solutions = []
-
     for weights, area in solutions:
         d = {}
-        i = 0
-        for method in methods_order:
+        for i, method in enumerate(methods_order):
             d[method] = weights[i]
-            i = i + 1
         l_solutions.append((d, area))
     return l_solutions
 
@@ -235,23 +210,13 @@ def grid_optimize(func, low_quality=set()):
     constraint 4: w_fml <= 0.5
     constraint 5*: w_i <= 0.05 for all w_i may not apply if some w_i is discarded
 
-    The common order of methods is this one:
-    "oncodriveclustl": w[0],
-    "dndscv": w[1],
-    "oncodrivefml": w[2],
-    "hotmaps": w[3],
-    "smregions": w[4],
-    "cbase": w[5]
-
     """
-    optimum = {'Objective_Function': 0, 'oncodriveclustl': None, 'dndscv': None, 'oncodrivefml': None,
-               'hotmaps': None, 'cbase': None, "smregions":None}
 
-    methods_list = ['oncodriveclustl', 'dndscv', 'oncodrivefml', 'hotmaps','smregions', 'cbase']
+    optimum = {k: None for k in METHODS}.update({'Objective_Function': 0})
     low_quality_index = None
     if len(low_quality) > 0:
-        low_quality_index = [methods_list.index(v) for v in low_quality]
-    low = [0.05 if v not in low_quality else 0 for v in methods_list]  # change lower bound on discarded methods
+        low_quality_index = [METHODS.index(v) for v in low_quality]
+    low = [0.05 if v not in low_quality else 0 for v in METHODS]  # change lower bound on discarded methods
     for w in itertools.product(np.linspace(0, 1, 21), repeat=5):
         if sum(w) <= 0.95:  # belongs to simplex
             w = list(np.append(w, [1 - sum(w)]))
@@ -268,12 +233,8 @@ def grid_optimize(func, low_quality=set()):
                                 f = func(w)
                                 if optimum['Objective_Function'] > f:
                                     optimum['Objective_Function'] = f
-                                    optimum['oncodriveclustl'] = w[0]
-                                    optimum['dndscv'] = w[1]
-                                    optimum['oncodrivefml'] = w[2]
-                                    optimum['hotmaps'] = w[3]
-                                    optimum['smregions'] = w[4]
-                                    optimum['cbase'] = w[5]
+                                    for i, v in enumerate(METHODS):
+                                        optimum[v] = w[i]
     return optimum
 
 
@@ -290,12 +251,11 @@ def create_scipy_constraints(low_quality=set()):
     # constraint 4: dndscv <= 0.5
     # constraint 5: fml <= 0.5
 
-    methods_list = ['oncodriveclustl', 'dndscv', 'oncodrivefml', 'hotmaps', 'smregions', 'cbase']
     cons = [{'type': 'eq', 'fun': simplex_bound},
             {'type': 'ineq', 'fun': clustering_bound},
             {'type': 'ineq', 'fun': recurrence_bound},
             {'type': 'ineq', 'fun': fm_bound}]
-    for ind, v in enumerate(methods_list):
+    for ind, v in enumerate(METHODS):
         if v in low_quality:
             cons += [{'type': 'eq', 'fun': partial(array_component, i=ind)}]
         else:
@@ -305,9 +265,8 @@ def create_scipy_constraints(low_quality=set()):
 
 
 def satisfy_constraints(w, low_quality=set()):
-    methods_list = ['oncodriveclustl', 'dndscv', 'oncodrivefml', 'hotmaps', 'smregions', 'cbase']
     satisfy = True
-    for i, v in enumerate(methods_list):
+    for i, v in enumerate(METHODS):
         if v in low_quality:
             satisfy = satisfy and (abs(array_component(w, i)) < 0.01)
         else:
@@ -338,7 +297,6 @@ def optimize_with_seed(func, w0, low_quality=set()):
     return res
 
 
-
 def full_optimizer(cancer, input_rankings, method_reject, moutput, percentage_cgc, seed):
     '''
 
@@ -351,60 +309,59 @@ def full_optimizer(cancer, input_rankings, method_reject, moutput, percentage_cg
     :return:
     '''
 
-    global gavaliable_methods, order_methods, gt_combination
+    global gavaliable_methods
     if seed == 'T':
         np.random.seed(1)
+
     # Select order methods
-    gt_combination = "RANKING"
-    order_methods = order_methods_ranking
     with gzip.open(input_rankings, "rb") as fd:
         d_results_methodsr = pickle.load(fd)
     print(d_results_methodsr.keys())
+
     # Prepare the list of available methods and the dictionary of weights
     l = []
     for method in d_results_methodsr.keys():
         l.append(method)
     gavaliable_methods = list(l)
+
     # Remove methods that do not reach the quality metrics
     if not (method_reject is None):
         discarded = set()
         discarded.add(method_reject)
     else:
         discarded = set(["{}".format(m) for m in Filter(input_run=moutput, tumor=cancer).filters()])
+
     # Include discarded from command line
     if len(discarded) > 0:
         print("[QC] {} discarded {}".format(cancer, discarded))
     gavaliable_methods = [m for m in gavaliable_methods if m not in discarded]
     print("Running on " + str(gavaliable_methods))
+
     # Set to empty those methods discarded or not present
-    for method in order_methods:
+    for method in METHODS:
         if method not in gavaliable_methods:
             d_results_methodsr[method] = {}
     print(d_results_methodsr)
+
     # Instantiate the enrichment objective function
     objective_function = Evaluation_Enrichment(percentage_cgc)
-    f = partial(calculate_objective_function, d_results_methodsr, objective_function=objective_function)
-    if gt_combination == "RANKING":
-        def func(w):
-            return -f({"oncodriveclustl": w[0],
-                       "dndscv": w[1],
-                       "oncodrivefml": w[2],
-                       "hotmaps": w[3],
-                       "smregions": w[4],
-                       "cbase": w[5]})
+    f = partial(calculate_objective_function, d_results_methodsr, objective_function)
 
-    all_methods = ['oncodriveclustl', 'dndscv', 'oncodrivefml', 'hotmaps', 'smregions', 'cbase']
+    def func(w):
+        return -f(dict(zip(METHODS, w)))
+
     # best solution in 1/20 resolution grid, augmented with basin-hopping/SLSQP optimization
     grid_optimum = grid_optimize(func, low_quality=discarded)  # get optimum candidate in the grid
-    w = np.array([grid_optimum[k] for k in all_methods])
+    w = np.array([grid_optimum[k] for k in METHODS])
     res = optimize_with_seed(func, w)  # basin-hopping/SLSQP using grid optimum candidate as initial guess
-    res_dict = dict(zip(all_methods, list(res.x)))
+    res_dict = dict(zip(METHODS, list(res.x)))
     res_dict['Objective_Function'] = res.fun
+
     # choose the best one grid or basin-hopping, unless basin-hopping does not fulfill the constraints
     if res_dict['Objective_Function'] > grid_optimum['Objective_Function']:
         out_df = pd.DataFrame({k: [v] for k, v in grid_optimum.items()})
     else:
-        r = np.array([res_dict[k] for k in all_methods])
+        r = np.array([res_dict[k] for k in METHODS])
         if satisfy_constraints(r, low_quality=discarded):
             out_df = pd.DataFrame({k: [v] for k, v in res_dict.items()})
         else:
@@ -412,11 +369,11 @@ def full_optimizer(cancer, input_rankings, method_reject, moutput, percentage_cg
     return out_df
 
 
-def skip_optimizer(input_rankings, method_reject, moutput, cancer):
+def skip_optimizer(method_reject, moutput, cancer):
 
     global gavaliable_methods
 
-    gavaliable_methods = ['oncodriveclustl', 'dndscv', 'oncodrivefml', 'hotmaps', 'smregions', 'cbase']
+    gavaliable_methods = METHODS
 
     # Remove methods that do not reach the quality metrics
     if not (method_reject is None):
@@ -424,9 +381,11 @@ def skip_optimizer(input_rankings, method_reject, moutput, cancer):
         discarded.add(method_reject + "_r")
     else:
         discarded = set(["{}_r".format(m) for m in Filter(input_run=moutput, tumor=cancer).filters()])
+
     # Include discarded from command line
     gavaliable_methods = [m for m in gavaliable_methods if m not in discarded]
     print("Running on " + str(gavaliable_methods))
+
     # Create a uniform vector of weights
     N = len(gavaliable_methods)
     df = pd.DataFrame({k: [1/N] for k in gavaliable_methods})
@@ -467,12 +426,13 @@ def skip_optimizer(input_rankings, method_reject, moutput, cancer):
               type=str,
               help="Whether a seed for random generation should be defined. Default T=True. [T=True,F=False]",
               required=True)
-def run_optimizer( foutput, input_rankings, percentage_cgc, moutput, cancer, seed, method_reject):
+def run_optimizer(foutput, input_rankings, percentage_cgc, moutput, cancer, seed, method_reject):
 
     if float(percentage_cgc) > 0.0:
         out_df = full_optimizer(cancer, input_rankings, method_reject, moutput, percentage_cgc, seed)
     else:
-        out_df = skip_optimizer(input_rankings,method_reject,moutput,cancer)
+        out_df = skip_optimizer(method_reject, moutput, cancer)
+
     # write to table
     out_df.to_csv(foutput, sep="\t", index=False, compression="gzip")
 
