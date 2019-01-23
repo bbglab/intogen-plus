cccdb39ba1f971dd58bf7352d9e47cc398601c6b
franmj <francisco.martinez@irbbarcelona.org>
2019-01-23 10:03:41 +0100

    Clean code of stouffer and create tier drivers

---
+++ datasets_build/cgc/download_cgc.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+import bglogs
+import click
+import requests
+
+from homura import download
+
+CGC_URL = "https://cancer.sanger.ac.uk/cosmic/file_download/GRCh37/cosmic/v87/cancer_gene_census.csv" # Currently version v87
+COSMIC_KEY = os.getenv("COSMIC_KEY", None)
+
+
+@click.command()
+@click.option('--download', 'download_folder', help='Download folder')
+@click.option('--debug', is_flag=True)
+def cmdline(download_folder, debug=False):
+    bglogs.configure(debug=debug)
+
+    if COSMIC_KEY is None:
+        bglogs.error("Environment variable COSMIC_KEY not set")
+        bglogs.error("Define your key like this:\n\texport COSMIC_KEY=$(echo \"email@example.com:mycosmicpassword\" | base64)")
+        sys.exit(-1)
+
+    output_folder = os.path.join(download_folder, 'cgc')
+    os.makedirs(output_folder, exist_ok=True)
+    output_file = os.path.join(output_folder, 'cancer_gene_census.csv')
+
+    r = requests.get(CGC_URL, headers={"Authorization": "Basic {}".format(COSMIC_KEY)})
+    url = r.json()['url']
+    bglogs.debug(url)
+
+    if os.path.exists(output_file):
+        os.unlink(output_file)
+
+    download(url, path=output_file)
+
+
+if __name__ == "__main__":
+    cmdline()
