6d388a8c91f18978318f7c60e6747e9c1ba95f4a
franmj <francisco.martinez@irbbarcelona.org>
2019-01-31 10:51:59 +0100

    Included creation of role for Framework2

---
+++ datasets_build/role/run_role.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -xe
+
+if [ -z "${INTOGEN_DATASETS}" ]
+then
+      echo "ERROR: Define the INTOGEN_RELEASE variable"
+      exit -1
+fi
+
+
+# define the paths
+path_base=$INTOGEN_DATASETS/role/
+path_output=$path_base/drivers_role.tsv
+# Datasets
+path_base_intogen=/workspace/projects/intogen_2017/
+path_cgi=$path_base_intogen/data/latest/cgi/gene_MoA.tsv
+path_intogen_drivers=$path_base_intogen/analyses/2018_12_12/vetted_drivers05.tsv
+dnds_input_path=/workspace/projects/ubiquitins/run_tcga_pan/intogen_runs/20181105/dndscv/PCATLAS_WXS_PAN.out.gz
+
+python calculate_consensous_role.py --path_cgi_moa $path_cgi --path_drivers $path_intogen_drivers --path_run_dndscv $dnds_input_path \
+                                    --path_output $path_output
