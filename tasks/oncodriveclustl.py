7ea71549486932888e044cb977a7910e50f52268
Loris Mularoni <loris.mularoni@irbbarcelona.org>
2019-02-22 09:32:07 +0100

    OncodriveCLUSTL uses global signatures
    Former-commit-id: 4fc57c9340f28fe63e20de660d65879cd108fd9f

--- tasks/oncodriveclustl.py
+++ tasks/oncodriveclustl.py
@@ -35,10 +35,11 @@ class OncodriveClustlTask(Task):
         cores = os.environ.get("INTOGEN_CPUS", 4)
         genome = os.environ['INTOGEN_GENOME']
         kmer = 5 if self.CANCER_TYPE == 'SKCM' else 3
+        signatures = '' if self.signatures_file is None else f'-sign {self.signatures_file}'
 
         run_command(f"""
             {self.cmdline} -i {self.in_file} -o {self.output_folder}/{self.name} -r {cds_regions}  
-            -c {cores} -g {genome} -sim region_restricted -n 10000 -kmer {kmer} --concatenate && 
+            -c {cores} -g {genome} -sim region_restricted -n 10000 -kmer {kmer} {signatures} --concatenate && 
             (cat {self.output_folder}/{self.name}/elements_results.txt | gzip > {self.out_file}) && 
             (cat {self.output_folder}/{self.name}/clusters_results.tsv | gzip > {self.output_folder}/{self.name}.clusters.gz) &&  
             (cat {self.output_folder}/{self.name}/oncohortdrive_results.out | gzip > {self.output_folder}/{self.name}.oncohortdrive.gz) 
