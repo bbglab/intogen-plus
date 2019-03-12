768a7d0865ca03a55a1730213aefe11ca9c2f374
Loris Mularoni <loris.mularoni@irbbarcelona.org>
2019-03-12 12:47:01 +0100

    Updated deconstructsig
    Former-commit-id: 1a5e8cbb43bdd9133813c02382488f79c286b7e5

--- containers_build/deconstructsig/run_deconstruct.py
+++ containers_build/deconstructsig/run_deconstruct.py
@@ -40,14 +40,31 @@ def run_deconstruct(input_file, weights, build):
     muts = muts[(muts['REF'] != '-') & (muts['REF'].isin(list('ACGT')))]
     muts = muts[(muts['ALT'] != '-') & (muts['ALT'].isin(list('ACGT')))]
 
+    # samples
+    samples = set(muts['SAMPLE'].values)
+
     # filter out mutations in driver genes
-    print(cohort)
     df_filtered = pd.read_csv(DRIVERS_PATH, sep="\t")
     df_filtered = df_filtered[df_filtered["COHORT"] == cohort]
+    if df_filtered.shape[0] > 0:
+        list_drivers = df_filtered["SYMBOL"].unique()
+        muts = muts[~(muts["GENE"].isin(list_drivers))]
 
-    # if df_filtered.shape[0] > 0:
-    #     list_drivers = df_filtered["SYMBOL"].unique()
-    #     muts = muts[~(muts["GENE"].isin(list_drivers))]
+    # make sure we did not remove a sample entirely; if we did, add at least one
+    # mutation of the most abundant context in the cohort.
+    profile = muts.groupby(by='MUTATION_TYPE').count()
+    profile = dict(zip(list(profile.index), list(profile.iloc[:, 0])))
+    most_frequent = max(profile, key=profile.get)
+    mutsamples = list(muts['SAMPLE'].values)
+    mutation = muts[muts['MUTATION_TYPE'] == most_frequent].iloc[0, :]
+    concat_pool = []
+    for sample in samples:
+        if sample not in mutsamples:
+            df = pd.DataFrame(columns=muts.columns, index=[0])
+            df.iloc[0, :] = mutation
+            df.loc['SAMPLE'] = sample
+            concat_pool.append(df)
+    muts = pd.concat([muts] + concat_pool, axis=0)
 
     # keep mutations in a temporal file
     muts.to_csv(MUTS_PATH, sep='\t', index=False)
@@ -55,7 +72,6 @@ def run_deconstruct(input_file, weights, build):
     # run deconstructSigs
     command = 'Rscript ' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'deconstructSigs.r') + \
               ' ' + MUTS_PATH + ' ' + build + ' ' + weights_r
-
     print(command)
     r = subprocess.run(command, shell=True, executable='/bin/bash')
     if r.returncode != 0:
