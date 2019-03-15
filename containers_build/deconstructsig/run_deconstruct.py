b9eb70c386ae921f0c0dbb0dd73017a1d46f46bc
Loris Mularoni <loris.mularoni@irbbarcelona.org>
2019-03-15 14:18:30 +0100

    Fixed bug in deconstructsigs
    Former-commit-id: 05f4641d3fcb42fe2187f50fb4d6a166c34cc680

--- containers_build/deconstructsig/run_deconstruct.py
+++ containers_build/deconstructsig/run_deconstruct.py
@@ -50,25 +50,27 @@ def run_deconstruct(input_file, weights, build):
     # filter out mutations in driver genes
     df_filtered = pd.read_csv(DRIVERS_PATH, sep="\t")
     df_filtered = df_filtered[df_filtered["COHORT"] == cohort]
+    muts_copy = muts.copy()
     if df_filtered.shape[0] > 0:
         list_drivers = df_filtered["SYMBOL"].unique()
         muts = muts[~(muts["GENE"].isin(list_drivers))]
 
-    # make sure we did not remove a sample entirely; if we did, add at least one
-    # mutation of the most abundant context in the cohort.
+    # check if we remove a sample entirely by filtering;
+    # should we do so, we must keep at least one mutation for this sample;
+    # we rescue the one with highest frequency in the cohort profile.
     profile = muts.groupby(by='MUTATION_TYPE').count()
     profile = dict(zip(list(profile.index), list(profile.iloc[:, 0])))
-    most_frequent = max(profile, key=profile.get)
     mutsamples = list(muts['SAMPLE'].values)
-    mutation = muts[muts['MUTATION_TYPE'] == most_frequent].iloc[0, :]
     concat_pool = []
     for sample in samples:
         if sample not in mutsamples:
-            df = pd.DataFrame(columns=muts.columns, index=[0])
-            df.iloc[0, :] = mutation
-            df.loc['SAMPLE'] = sample
+            contexts_in_sample = muts_copy[muts_copy['SAMPLE'] == sample]['MUTATION_TYPE'].unique()
+            profile_in_sample = {c: profile[c] for c in contexts_in_sample}
+            max_c = max(profile_in_sample, key=profile_in_sample.get)
+            mutation = muts_copy[(muts_copy['MUTATION_TYPE'] == max_c) & (muts_copy['SAMPLE'] == sample)].iloc[0, :]
+            df = mutation.to_frame().transpose()
             concat_pool.append(df)
-    muts = pd.concat([muts] + concat_pool, axis=0)
+    muts = pd.concat([muts] + concat_pool)
 
     # keep mutations in a temporal file
     muts.to_csv(MUTS_PATH, sep='\t', index=False)
