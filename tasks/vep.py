798e6aa11b0e89fd57c0a4f81e2acde343907fc7
Loris Mularoni <loris.mularoni@irbbarcelona.org>
2019-03-19 15:15:59 +0100

    Removed zcat
    Former-commit-id: eb8d0d838bf9dcc8f2c34318a4f7af97c219efac

--- tasks/vep.py
+++ tasks/vep.py
@@ -35,7 +35,7 @@ class VepTask(Task):
         assembly = convert_genome_reference_name(os.environ['INTOGEN_GENOME'])
         
         run_command(f"""
-            {cmdline} -i <(zcat {self.in_file}) -o STDOUT --assembly {assembly} --no_stats --cache --offline --symbol --protein --tab --canonical --dir {self.datasets} | grep -v ^## | gzip > {self.out_file}; 
+            {cmdline} -i {self.in_file} -o STDOUT --assembly {assembly} --no_stats --cache --offline --symbol --protein --tab --canonical --dir {self.datasets} | grep -v ^## | gzip > {self.out_file}; 
             mkdir -p vep/logs; mv STDOUT_warnings.txt vep/logs/{self.name}.log; 
         """)
 
