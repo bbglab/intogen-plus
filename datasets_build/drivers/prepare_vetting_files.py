79e0e50c2586db7dcfdac5223791989b0aae1601
franmj <francisco.martinez@irbbarcelona.org>
2019-04-06 12:06:20 +0200

    Changes in driver calling
    Former-commit-id: 5eb4dfca3bcccaf68052eca0041fee7994dd9e3e

--- datasets_build/drivers/prepare_vetting_files.py
+++ datasets_build/drivers/prepare_vetting_files.py
@@ -203,7 +203,7 @@ def main(paths,pattern,n_muts_gene,info_cohorts,output,expression_file_tcga,exac
     df_info_cohort = pd.read_csv(info_cohorts, sep="\t")
     # Add information of cancer type
     df_final_signatures_info = pd.merge(df_final_signatures, df_info_cohort)
-    df_counts = df_final_signatures_info.groupby("SYMBOL", as_index=False).agg({"COHORT": "count"})
+    df_counts = df_final_signatures_info.groupby("GENE", as_index=False).agg({"COHORT": "count"})
     df_counts.rename(columns={"COHORT": "num_cohorts"}, inplace=True)
     df_final_signatures_info = df_final_signatures_info.merge(df_counts)
     df_final_signatures_info["Warning_num_cohorts"] = df_final_signatures_info.apply(
@@ -220,7 +220,7 @@ def main(paths,pattern,n_muts_gene,info_cohorts,output,expression_file_tcga,exac
         artifacts = json.load(f)
     df_final_signatures_info["Warning_Artifact"] = df_final_signatures_info.apply(lambda row: row["GENE"] in artifacts["suspects"],axis=1)
     # Add warning of number of cohorts per gene
-    df_counts = df_final_signatures_info.groupby("SYMBOL",as_index=False).agg({"COHORT":"count"})
+    df_counts = df_final_signatures_info.groupby("GENE",as_index=False).agg({"COHORT":"count"})
     df_counts.rename(columns={"COHORT":"num_cohorts"},inplace=True)
     df_final_signatures_info=df_final_signatures_info.merge(df_counts)
     df_final_signatures_info["Warning_num_cohorts"] = df_final_signatures_info.apply(lambda row:True if row["num_cohorts"] == 1 else False,axis=1)
