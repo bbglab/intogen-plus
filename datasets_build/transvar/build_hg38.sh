fd88e5eb6f055a135eb5242d98b60e8915b1baff
Loris Mularoni <loris.mularoni@irbbarcelona.org>
2019-10-15 12:51:45 +0200

    Updated the scripts to create the transvar datasets
    Former-commit-id: f1fcb100af718ec7039578b2617e6e7e9974b4b5

--- datasets_build/transvar/build_hg38.sh
+++ datasets_build/transvar/build_hg38.sh
@@ -2,17 +2,12 @@
 
 set -xe
 
-export INTOGEN_GENOME="hg38"
-export INTOGEN_VEP="vep92"
-export INTOGEN_RELEASE="develop"
-
-
 ENSEMBL_RELEASE=${INTOGEN_VEP/vep/}
 ENSEMBL_REFERENCE=${INTOGEN_GENOME/hg/GRCh}
 ENSEMBL_REFERENCE=${ENSEMBL_REFERENCE/GRCh19/GRCh37}
 
 GENOME="Homo_sapiens.${ENSEMBL_REFERENCE}.fa"
-echo "Build genome fasta file"
+echo -e "\tBuild genome fasta file"
 for f in $(ls ../../datasets/${INTOGEN_GENOME}_${INTOGEN_VEP}_${INTOGEN_RELEASE}/bgdata/datasets/genomereference/hg38-20161209/chr*.txt); do 
     basename $f | sed 's/chr/>/g' | sed 's/.txt//g' >> ${GENOME}
     fold -w61 $f | awk '{printf "%61s\n", $0}' >> ${GENOME}
@@ -21,33 +16,23 @@ done
 # Index fasta file
 TRANSVAR_DATA="../../datasets/${INTOGEN_GENOME}_${INTOGEN_VEP}_${INTOGEN_RELEASE}/transvar"
 mkdir -p $TRANSVAR_DATA
-TRANSVAR="singularity run -B ${TRANSVAR_DATA}:/data ../../containers/${INTOGEN_RELEASE}/transvar.simg"
+TRANSVAR="singularity run -B ${TRANSVAR_DATA}:/data ../../containers/${INTOGEN_GENOME}_${INTOGEN_VEP}_${INTOGEN_RELEASE}/transvar.simg"
 
-echo "Indexing fasta file"
+echo -e "\tIndexing fasta file"
 $TRANSVAR index --reference ${GENOME}
 
-echo "Configure genome reference"
+echo -e "\tConfigure genome reference"
 mv ${GENOME}* $TRANSVAR_DATA
 $TRANSVAR config -k reference -v /data/${GENOME} --refversion ${ENSEMBL_REFERENCE}
 
-echo "Downloading ENSEMBL GTF"
+echo -e "\tDownloading ENSEMBL GTF"
 ENSEMBL_GTF="Homo_sapiens.${ENSEMBL_REFERENCE}.${ENSEMBL_RELEASE}.gtf.gz"
 wget "ftp://ftp.ensembl.org/pub/release-${ENSEMBL_RELEASE}/gtf/homo_sapiens/${ENSEMBL_GTF}"
 
-echo "Indexing ENSEMBL"
+echo -e "\tIndexing ENSEMBL"
 $TRANSVAR index --ensembl ${ENSEMBL_GTF}
 
-echo "Configure ENSEMBL"
+echo -e "\tConfigure ENSEMBL"
 mv ${ENSEMBL_GTF}.transvardb* $TRANSVAR_DATA/
 rm ${ENSEMBL_GTF}*
 $TRANSVAR config -k ensembl -v /data/${ENSEMBL_GTF}.transvardb --refversion ${ENSEMBL_REFERENCE}
-
-
-
-
-
-
-
-
-
-
