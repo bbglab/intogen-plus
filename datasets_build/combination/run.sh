07faba12d773658523078dfdce6a72681a7b30b0
Loris Mularoni <loris.mularoni@irbbarcelona.org>
2019-10-22 14:41:07 +0200

    Updated script to create the combination datasets
    Former-commit-id: 11732bc146bbd49bf467e02abf43a4191525a6d4

--- datasets_build/combination/run.sh
+++ datasets_build/combination/run.sh
@@ -1,5 +1,5 @@
 # Define your key like this:
-export COSMIC_KEY=$(echo "email@example.com:mycosmicpassword" | base64)
+# export COSMIC_KEY=$(echo "email@example.com:mycosmicpassword" | base64)
 
 ./run_negative_set.sh
 ./run_cgc.sh
