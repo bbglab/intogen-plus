87c3b9bbdece54c4615a1b2663008ce064d1b035
Loris Mularoni <loris.mularoni@irbbarcelona.org>
2019-10-24 15:41:32 +0200

    Added environmental variable requirements
    Former-commit-id: c3d74321a7b4da181af579dc60490420ce369a46

--- datasets_build/cbase/run.sh
+++ datasets_build/cbase/run.sh
@@ -1,3 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "${INTOGEN_RELEASE}" ]
+then
+      echo "ERROR: Define the INTOGEN_RELEASE variable"
+      exit -1
+fi
+
+if [ -z "${INTOGEN_VEP}" ]
+then
+      echo "ERROR: Define the INTOGEN_VEP variable"
+      exit -1
+fi
+
+if [ -z "${INTOGEN_GENOME}" ]
+then
+      echo "ERROR: Define the INTOGEN_GENOME variable"
+      exit -1
+fi
+
+
 wget -c http://genetics.bwh.harvard.edu/cbase/CBaSE_v1.1.zip
 unzip CBaSE_v1.1.zip
 cp CBaSE_v1.1/Auxiliary/*.gz ../../datasets/${INTOGEN_GENOME}_${INTOGEN_VEP}_${INTOGEN_RELEASE}/cbase/.
