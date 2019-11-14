dab0a661d0336a3670e8caaff3b71c11292ab168
franmj <francisco.martinez@irbbarcelona.org>
2019-11-14 12:24:22 +0100

    combine using mutpanning
    Former-commit-id: 645676c62fe5218c41b9dd88f2bd0f19e130f22e

--- tasks/combination.py
+++ tasks/combination.py
@@ -9,7 +9,7 @@ from .base import Task, run_command
 
 class CombinationTask(Task):
 
-    KEY = 'combination'
+    KEY = 'combination_mutpanning'
 
     def run(self):
         run_command(f"{self.cmdline} {self.output_folder} {self.name}")
