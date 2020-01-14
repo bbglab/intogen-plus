bb424928b1efd8734de12f331d8bd4d9adeb79d2
I. Reyes <iker.reyes@irbbarcelona.org>
2020-01-14 10:54:26 +0100

    Fixed deprecated UCSC path and missing .master file
    Former-commit-id: ffab6090eaf45a4de67e6a5010ac876ae93f6fbb

--- datasets_build/preprocess/run.sh
+++ datasets_build/preprocess/run.sh
@@ -24,12 +24,12 @@ echo -e "\tDownloading liftover chain files"
 PREPROCESS=../../datasets/${INTOGEN_GENOME}_${INTOGEN_VEP}_${INTOGEN_RELEASE}/preprocess
 mkdir -p ${PREPROCESS}
 wget -c http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz -O ${PREPROCESS}/hg19ToHg38.over.chain.gz
-wget -c http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg38ToHg19.over.chain.gz -O ${PREPROCESS}/hg38ToHg19.over.chain.gz
+wget -c http://hgdownload.soe.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz -O ${PREPROCESS}/hg38ToHg19.over.chain.gz
 
 echo -e "\tDownloading pileup mappability files"
 bgdata get --force intogen/coverage/${INTOGEN_GENOME}
-# Redo the download (it will not download the data again) to force to get the hg38.master file
-bgdata get datasets/genomereference/${INTOGEN_GENOME}
+# Redo the download (it will not download the data again) to force to get the .master file
+bgdata get --force intogen/coverage/${INTOGEN_GENOME}
 COVERAGE=../../datasets/${INTOGEN_GENOME}_${INTOGEN_VEP}_${INTOGEN_RELEASE}/bgdata/intogen/coverage
 LAST_VERSION=`cat ${COVERAGE}/${INTOGEN_GENOME}.master`
 cp ${COVERAGE}/${INTOGEN_GENOME}-${LAST_VERSION}/${INTOGEN_GENOME}_100bp.coverage.regions.gz ${PREPROCESS}/.
