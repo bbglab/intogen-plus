d5b348fcb15b2b9a0f5a7e71d3e5d6d02cf3fa9b
I. Reyes <iker.reyes@irbbarcelona.org>
2020-03-26 16:09:07 +0100

    Updated transvar container
    Former-commit-id: 7cd394717dedfb33bed747acf9d7d9c4d2a54562

--- containers_build/transvar/transvar.mk
+++ containers_build/transvar/transvar.mk
@@ -1,7 +1,21 @@
 
+#CONTAINER_TRANSVAR = $(CONTAINERS)/transvar.simg
+#
+#$(CONTAINER_TRANSVAR): | $(CONTAINERS)
+#	singularity build $@ docker://zhouwanding/transvar
+#
+#TARGETS_CONTAINERS += $(CONTAINER_TRANSVAR)
+
+
 CONTAINER_TRANSVAR = $(CONTAINERS)/transvar.simg
 
-$(CONTAINER_TRANSVAR): | $(CONTAINERS)
-	singularity build $@ docker://zhouwanding/transvar
+SRC_CONTAINER_TRANSVAR = ${CONTAINERS_SOURCE_FOLDER}/transvar
 
-TARGETS_CONTAINERS += $(CONTAINER_TRANSVAR)
+$(CONTAINER_TRANSVAR): ${SRC_CONTAINER_TRANSVAR}/Singularity | $(CONTAINERS)
+	@echo Building TRANSVAR container
+	cd ${SRC_CONTAINER_TRANSVAR} && \
+		sudo singularity build $$(basename $@) Singularity
+	mv ${SRC_CONTAINER_TRANSVAR}/$$(basename $@) $@
+	sudo chown ${USER}: $@
+
+TARGETS_CONTAINERS_SUDO += $(CONTAINER_TRANSVAR)
