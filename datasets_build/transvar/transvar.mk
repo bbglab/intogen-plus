aa1e750319d0637e563a3b8bb92c99308bde66e3
I. Reyes <iker.reyes@irbbarcelona.org>
2020-03-26 15:56:19 +0100

    Updated transvar dataset build
    Former-commit-id: 05b0ef272e0128e387915e025321710fce490d6e

--- datasets_build/transvar/transvar.mk
+++ datasets_build/transvar/transvar.mk
@@ -1,26 +1,40 @@
 
-SRC_TRANSVAR = ${DATASETS_SOURCE_FOLDER}/transvar
+SRC_DATASETS_TRANSVAR = ${DATASETS_SOURCE_FOLDER}/transvar
 
-TRANSVAR_FOLDER ?= $(DATASETS)/transvar
-$(TRANSVAR_FOLDER): | $(DATASETS)
+DATASETS_TRANSVAR ?= $(DATASETS)/transvar
+$(DATASETS_TRANSVAR): | $(DATASETS)
 	mkdir $@
 
 # FIXME for other genomes than hg38, the GRCh version may differ
-GENOME_FASTA = $(TRANSVAR_FOLDER)/Homo_sapiens.GRCh${GENOME}.fa
-$(GENOME_FASTA): ${SRC_TRANSVAR}/build_fasta.sh | $(TRANSVAR_FOLDER)
+GENOME_FASTA = $(DATASETS_TRANSVAR)/Homo_sapiens.GRCh${GENOME}.fa
+$(GENOME_FASTA): ${SRC_DATASETS_TRANSVAR}/build_fasta.sh | $(DATASETS_TRANSVAR)
 	@echo Build genome fasta file
-	bash ${SRC_TRANSVAR}/build_fasta.sh hg${GENOME} $@
+	bash ${SRC_DATASETS_TRANSVAR}/build_fasta.sh hg${GENOME} $@
 
 
+GENOME_FASTA_INDEX = $(GENOME_FASTA).fai
+$(GENOME_FASTA_INDEX): $(GENOME_FASTA) $$(CONTAINER_TRANSVAR) | $(DATASETS_TRANSVAR)
+	@echo Indexing genome fasta file
+	singularity run -B $(DATASETS_TRANSVAR):/data $(CONTAINER_TRANSVAR) \
+		index --reference /data/$$(basename $<)
+	singularity run -B $(DATASETS_TRANSVAR):/data $(CONTAINER_TRANSVAR) \
+		config -k reference -v /data/Homo_sapiens.GRCh${GENOME}.fa \
+		--refversion GRCh${GENOME}
 
-ENSEMBL_GTF = $(TRANSVAR_FOLDER)/Homo_sapiens.GRCh${GENOME}.ENSEMBL.gtf.gz
-$(ENSEMBL_GTF): | $(TRANSVAR_FOLDER)
+ENSEMBL_GTF = $(DATASETS_TRANSVAR)/Homo_sapiens.GRCh${GENOME}.${ENSEMBL}.gtf.gz
+$(ENSEMBL_GTF): | $(DATASETS_TRANSVAR)
 	@echo Downloading ENSEMBL GTF
-	wget "ftp://ftp.ensembl.org/pub/release-${ENSEMBL_RELEASE}/gtf/homo_sapiens/${ENSEMBL_GTF}" \
+	wget "ftp://ftp.ensembl.org/pub/release-${ENSEMBL}/gtf/homo_sapiens/Homo_sapiens.GRCh${GENOME}.${ENSEMBL}.gtf.gz" \
 		-O $@
 
-#ALL_TARGETS+=$(ENSEMBL_GTF)
+ENSEMBL_INDEX = $(ENSEMBL_GTF).transvardb
+$(ENSEMBL_INDEX): $(ENSEMBL_GTF) $(GENOME_FASTA_INDEX) $$(CONTAINER_TRANSVAR) | $(DATASETS_TRANSVAR)
+	@echo Configure genome reference
+	singularity run -B $(DATASETS_TRANSVAR):/data $(CONTAINER_TRANSVAR) \
+		index --ensembl /data/$$(basename $(ENSEMBL_GTF))
+	singularity run -B $(DATASETS_TRANSVAR):/data $(CONTAINER_TRANSVAR) \
+		config -k ensembl -v /data/$$(basename $(ENSEMBL_INDEX)) \
+		--refversion GRCh${GENOME}
 
-# TODO index fasta and ensembl and configure ensemble. Requies transvar image
-
-TARGETS_DATASETS += $(GENOME_FASTA)
+DATASETS_TRANSVAR_FILES = $(GENOME_FASTA) $(GENOME_FASTA_INDEX) $(ENSEMBL_GTF) $(ENSEMBL_INDEX)
+TARGETS_DATASETS += $(GENOME_FASTA) $(GENOME_FASTA_INDEX) $(ENSEMBL_GTF) $(ENSEMBL_INDEX)
