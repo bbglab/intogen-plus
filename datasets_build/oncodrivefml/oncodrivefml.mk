86ac619db4649d54ae3cfa94e158670d19caabd2
I. Reyes <iker.reyes@irbbarcelona.org>
2020-03-26 16:12:38 +0100

    Set local path for cadd build
    Former-commit-id: a54cc23f1e21c141987141deb9d6f994a122d120

--- datasets_build/oncodrivefml/oncodrivefml.mk
+++ datasets_build/oncodrivefml/oncodrivefml.mk
@@ -4,12 +4,15 @@ $(DATASETS_FML): | $(DATASETS)
 	mkdir $@
 
 # FIXME for other genomes than hg38, the GRCh version may differ
-CADD_URL = http://krishna.gs.washington.edu/download/CADD/v${CADD}/GRCh${GENOME}/whole_genome_SNVs.tsv.gz
+#CADD_URL = http://krishna.gs.washington.edu/download/CADD/v${CADD}/GRCh${GENOME}/whole_genome_SNVs.tsv.gz
+# TODO fix this
+CADD_URL = /workspace/datasets/CADD/v1.4/hg38/whole_genome_SNVs.tsv.gz
 CADD_SCORES = ${DATASETS_FML}/cadd.tsv.gz
 $(CADD_SCORES): $$(REGIONS_CDS) | $(DATASETS_FML)
 	@echo Building OncodriveFML datasets
 	zcat $(REGIONS_CDS) | tail -n +2 |\
 		awk -v cadd="${CADD_URL}" '{system("tabix "cadd" "$$1":"$$2"-"$$3)}' |\
+		awk 'BEGIN {FS="\t";OFS = FS};{ $$5=""; print }' |\
 		gzip > $@.tmp
 	zcat $@.tmp |\
 		sort --parallel=${CORES} -S 4G -k1,1 -k2,2n |\
