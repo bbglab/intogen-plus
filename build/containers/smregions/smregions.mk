69d9464bb9f3adb1961e766e9b3067a952b40f53
I. Reyes <iker.reyes@irbbarcelona.org>
2020-06-09 15:09:35 +0200

    Build smregions container from new repo
    Former-commit-id: 2e9c57462fd6f33c1e3b1eaf0bd49eeca4f3a670

--- build/containers/smregions/smregions.mk
+++ build/containers/smregions/smregions.mk
@@ -3,15 +3,12 @@ SMREGIONS_CONTAINER = $(INTOGEN_CONTAINERS)/smregions.simg
 
 smregions_container_srcdir = ${src_containers}/smregions
 
-smregions_code_folder = ${src_containers}/../../smregions
-
-smregions_container_src = $(wildcard ${smregions_code_folder}/*) \
-	 $(wildcard ${smregions_code_folder}/smregions/*) \
-	 ${smregions_container_srcdir}/smregions.conf \
+smregions_container_src = ${smregions_container_srcdir}/smregions.conf \
 	 ${smregions_container_srcdir}/Singularity
 
 $(SMREGIONS_CONTAINER): $(smregions_container_src) | $(INTOGEN_CONTAINERS)
 	@echo Building SMRegions container
 	${container_builder} ${smregions_container_srcdir} $@
+
 
 CONTAINERS_SUDO += $(SMREGIONS_CONTAINER)
