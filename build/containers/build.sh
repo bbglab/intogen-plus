eb154a2e4994c5497764dc8e5640ab4bfb77f507
Federica Brando <federica.brando@irbbarcelona.org>
2023-02-06 17:00:15 +0100

    edit: add build.sh without root permissions
    Former-commit-id: 0bad28025b933dd53682ef5a671cac7403bdb6c2

--- build/containers/build.sh
+++ build/containers/build.sh
@@ -6,6 +6,14 @@ IMAGE=$2
 
 tmpdir=`mktemp -d`
 
-(cd ${FOLDER} && sudo singularity build ${tmpdir}/$(basename ${IMAGE}) Singularity)
+## if you have root permission you can uncomment the following:
+
+# (cd ${FOLDER} && sudo singularity build ${tmpdir}/$(basename ${IMAGE}) Singularity)
+# mv ${tmpdir}/$(basename ${IMAGE}) ${IMAGE}
+# sudo chown ${USER}: ${IMAGE}
+
+## if you DO NOT have root permission then you could use the --fakeroot flag as long as your user is specified in the /etc/subuid and /etc/subgid files. See https://docs.sylabs.io/guides/3.6/admin-guide/user_namespace.html#user-namespaces-fakeroot for more info. 
+
+(cd ${FOLDER} && singularity build --fakeroot ${tmpdir}/$(basename ${IMAGE}) Singularity)
 mv ${tmpdir}/$(basename ${IMAGE}) ${IMAGE}
-sudo chown ${USER}: ${IMAGE}
+chown ${USER}: ${IMAGE}
