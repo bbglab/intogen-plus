50e0bd982f5c25e80e39de93ee287aa8fab8b878
Federica Brando <federica.brando@irbbarcelona.org>
2023-03-14 17:28:50 +0100

    add: new step for producing canonical.regions.gz in dataset/boostdm/saturation
    Former-commit-id: c5b5faa75f646697b9d69a6cbfe8f1ebd3905999

--- build/datasets/boostdm/boostdm.mk
+++ build/datasets/boostdm/boostdm.mk
@@ -36,4 +36,11 @@ $(BOOST_PHYLO): $(boostdm_data_src)/hg38.download.sh | $(boostdm_dir)
 	@echo Creating phylop datasets
 	$< $(boostdm_dir)
 
-DATASETS += $(BOOSTDM_PFAM) $(BOOST_INFO_FUNCTIONAL) $(BOOST_PHYLO) $(BOOST_SYMLINKS)
+## STEP 5 - canonical.regions.gz
+BOOST_ALL_REGION = $(boostdm_dir)/saturation/canonical.regions.gz
+$(BOOST_ALL_REGION): $(boostdm_data_src)/get_all_regions.py $$(BIOMART_CDS)| $(boostdm_dir)
+	@echo Creating phosphosite
+	mkdir -p $(boostdm_dir)/saturation
+	python $< --biomart_path $(BIOMART_CDS) --out $(boostdm_dir)/saturation
+
+DATASETS += $(BOOSTDM_PFAM) $(BOOST_INFO_FUNCTIONAL) $(BOOST_PHYLO) $(BOOST_SYMLINKS) $(BOOST_ALL_REGION)
