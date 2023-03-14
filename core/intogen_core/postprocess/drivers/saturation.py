ee6d4c40e64d27516c0b0b7afc4821e108c4e71b
Federica Brando <federica.brando@irbbarcelona.org>
2023-03-14 17:29:19 +0100

    fix: new region file + new output format
    Former-commit-id: c8d902b793c4d7656e7c0034f0bbdea728789d78

--- core/intogen_core/postprocess/drivers/saturation.py
+++ core/intogen_core/postprocess/drivers/saturation.py
@@ -4,10 +4,6 @@ import os
 import tabix
 
 
-def drivers_list(driver_df):
-    """return a list of unique drivers"""
-    
-    return driver_df['SYMBOL'].unique()
 
 def read_vep(vep_f):
     return tabix.open(vep_f)
@@ -21,25 +17,25 @@ def get(vep_file, chromosome, start, stop):
     for row in tb.query("{}".format(chromosome), start, stop):
         yield row 
 
-def saturation(vep_path, d_list, regions_df):
+def saturation(vep_path, driver_df, regions_df):
     """
     Generator 
     
-    :params vep_path: path where vep.tsv.gz is stored
-    :params d_list: list of drivers 
-    :regions_df: dataframe of cds.regions.gz
+    :params vep_path: int, path where vep.tsv.gz is stored
+    :params driver_df: pandas.DataFrame 
+    :regions_df: pandas.DataFrame, canonical.regions.gz
 
-    :yield: a tuple with driver and df
+    :yield: driver and df
     """
-    d = dict()
+    d_list = driver_df['SYMBOL'].unique()
 
     for driver in d_list:
-        reg = regions_df[regions_df.SYMBOL == driver].sort_values("ELEMENT")
+        reg = regions_df[regions_df.SYMBOL == driver].sort_values("GENE_ID")
 
         # for transcript
         l_cases = list()
-        for element in reg.ELEMENT.unique():
-            regions_t = reg[reg.ELEMENT == element].sort_values("START")
+        for element in reg.TRANSCRIPT_ID.unique():
+            regions_t = reg[reg.TRANSCRIPT_ID == element].sort_values("START")
 
             for i, r in regions_t.iterrows():
                 start = r["START"]
@@ -55,34 +51,22 @@ def saturation(vep_path, d_list, regions_df):
                                    'Impact','Distance', 'Strand', 'Flags', 'Symbol', 'Symbol source', 'HGNC_ID', 'Canonical', 'ENSP']
                                 )
             
-            yield (driver, df)
+            yield driver, df
 
-
-def write_out(data, drivers_df):
-    """writing output {gene}.{tumortype}.vep.gz"""
-
-    drivers_df.drop_duplicates(subset=['SYMBOL', 'CANCER_TYPE'], inplace= True)
-    dic = drivers_df.groupby('SYMBOL')['CANCER_TYPE'].apply(list).to_dict()
-
-    driver, df = data
-
-    for ttype in dic[driver]:
-        df.to_csv(f'{driver}.{ttype}.vep.gz', index=False, compression='gzip', sep='\t')
 
 @click.command()
 @click.option('--drivers', type=click.Path(exists=True), required=True)
 def cli(drivers):
 
     drivers_df = pd.read_csv(drivers, sep='\t', low_memory=False)
-    drivers_l = drivers_list(drivers_df)
 
-    r_path = os.path.join(os.environ['INTOGEN_DATASETS'], 'regions', 'cds.regions.gz')
+    r_path = os.path.join(os.environ['INTOGEN_DATASETS'], 'boostdm', 'saturation','canonical.regions.gz')
     regions_df = pd.read_csv(r_path, sep='\t', low_memory=False)
 
     vep_path = os.path.join(os.environ['INTOGEN_DATASETS'], 'vep', 'vep.tsv.gz')
 
-    for data in saturation(vep_path, drivers_l, regions_df):
-        write_out(data, drivers_df)
+    for driver, df in saturation(vep_path, drivers_df, regions_df):
+        df.to_csv(f'{driver}.vep.gz', index=False, compression='gzip', sep='\t')
 
 if __name__ == "__main__":
     cli()   
