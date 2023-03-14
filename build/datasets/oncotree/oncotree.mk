8b5e5368e2dc8a5354bd0f81f0814722c530d585
Federica Brando <federica.brando@irbbarcelona.org>
2023-03-14 17:13:41 +0100

    fix: oncotree definition file in datasets
    Former-commit-id: a1903cf5596c96e8aabcb526cdf1d1b1e0b7e19b

--- build/datasets/oncotree/oncotree.mk
+++ build/datasets/oncotree/oncotree.mk
@@ -11,5 +11,8 @@ ONCOTREE = $(oncotree_dir)/tree.tsv
 $(ONCOTREE): ${oncotree_datasets_srcdir}/tree.tsv | $(oncotree_dir)
 	cp $< $@
 
+ONCOTREE_DEF = $(oncotree_dir)/oncotree-definitions.json
+$(ONCOTREE_DEF): $(oncotree_datasets_srcdir)/dictionary_long_names.json | $(oncotree_dir)
+	cp $< $@
 
-DATASETS += $(ONCOTREE)
+DATASETS += $(ONCOTREE) $(ONCOTREE_DEF)
