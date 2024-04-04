efabe4e6ec8873341bcb243190f9eb7278b4fa6a
Federica Brando <federica.brando@irbbarcelona.org>
2024-04-04 23:13:18 +0200

    fix: use ens_id col from config to fix empty symbols
    - use ENSEMBL_ID column
    - formatted parser.py with ruff

--- combination/intogen_combination/qc/parser.py
+++ combination/intogen_combination/qc/parser.py
@@ -11,7 +11,6 @@ logger = logging.getLogger(__name__)
 
 
 class Parser:
-
     def __init__(self, method, gene_coordinates):
         """Initialize an instance of the Parser class
         :param method: str, method to parse
@@ -20,34 +19,34 @@ class Parser:
         """
         self.name = method
         self.gene_coordinates = gene_coordinates
-        self.gene_id = CONF[method]['GENE_ID']
-        self.pvalue = CONF[method]['PVALUE']
-        self.qvalue = CONF[method]['QVALUE']
+        self.gene_id = CONF[method]["GENE_ID"]
+        self.pvalue = CONF[method]["PVALUE"]
+        self.qvalue = CONF[method]["QVALUE"]
+        self.ensid = CONF[method]["ENSEMBL_ID"]
 
     def read(self, input_file):
-        """Read an output file. 
+        """Read an output file.
         :param input_file: path, file to read with results
         :return: pandas.DataFrame
         """
         try:
             df = pd.read_csv(input_file, header=0, sep="\t")
-        except OSError as e:
-            logger.warning('File {} not found'.format(input_file))
+        except OSError:
+            logger.warning("File {} not found".format(input_file))
             return None
 
-        #FIXME No gene-name fix
+        # Use ensembl ID if no gene-name
         if df[self.gene_id].isna().any():
-            df[self.gene_id] = df[self.gene_id].fillna(df['ENSID'])
-        
+            df[self.gene_id] = df[self.gene_id].fillna(df[self.ensid])
+
         assert not df[self.gene_id].isna().any()
+        
         # P-value
         try:
             df = df[np.isfinite(df[self.pvalue])]
         except Exception:
-            logger.warning('No finite p-value'.format(input_file))
+            logger.warning("No finite p-value for {}".format(input_file))
         df = df[[self.gene_id, self.pvalue, self.qvalue]]
-        df.rename(columns={
-            self.gene_id: 'GENE_ID', self.pvalue: 'PVALUE', self.qvalue: 'QVALUE'
-        }, inplace=True)
+        df.rename(columns={self.gene_id: "GENE_ID", self.pvalue: "PVALUE", self.qvalue: "QVALUE"}, inplace=True)
 
         return df
